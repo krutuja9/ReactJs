b83123affb7e9570d25d43df9167ed8b486dd1f1
krutuja9 <krutuja609@gmail.com>
2024-06-02 23:04:21 +0530

    initial commit

---
+++ createmodule/calculator.js
@@ -0,0 +1,9 @@
+let sumData = (a,b)=>{
+  return a +b;
+}
+
+let subData = (a,b) =>{
+  return a-b;
+}
+
+export   {sumData, subData};
---
+++ createmodule/index.js
@@ -0,0 +1,7 @@
+import {sumData,subData} from "./calculator.js";
+
+console.log(sumData (10,20))
+console.log(subData (10,5))
+
+// defult export
+// name export 
